e5e51ba2a5e7b88cb430cae0461a07e3e58db6af
mehedi hasan <{ID}+{username}@users.noreply.github.com>
2023-02-04 12:54:34 +0600

    lvflutter_blg_log_reg api done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illiminate\Support\Facades\Auth;
+// use App\Http\User;
+use App\Models\User;
+use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Auth as FacadesAuth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        // validate fields 
+        $attrs = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        // create user 
+        $user = User::create(
+            [
+                'name' => $attrs['name'],
+                'email' => $attrs['email'],
+                'password' => bcrypt($attrs['password'])
+            ]
+        );
+
+        // return user and token in response 
+        return response([
+            'user' => $user,
+            'token' => $user->createToken('secret')->plainTextToken
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+
+        // validate fields 
+        $attrs = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        // attenmt  login
+        if (Auth::attempt($attrs)) {
+            return response([
+                'message' => 'invalide credentials'
+            ], 403);
+        }
+
+
+        // return user and token in response 
+        return response([
+            'user' => auth()->user(),
+            'token' => auth()->user()->createToken('secret')->plainTextToken
+        ], 200);
+    }
+
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response([
+            'message' => 'Logout success'
+        ], 200);
+    }
+
+
+    public function user()
+    {
+        return response([
+            'user' => auth()->user()
+        ], 200);
+    }
+}
