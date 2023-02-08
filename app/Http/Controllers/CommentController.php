97a2904e3b9f449ed51e5bc4cbedb351cee029ec
mehedi hasan <{ID}+{username}@users.noreply.github.com>
2023-02-08 07:15:29 +0600

    lvflutterblog_api_part complete

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,97 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    //
+    // get all comments 
+    public function index($id)
+    {
+        $post = Post::find($id);
+
+        if (!$post) {
+            return response([
+                'message' => 'post not found'
+            ], 403);
+        }
+        return response([
+            'comments' => $post->comments()->with('user:id,name,image')->get()
+
+        ], 200);
+    }
+
+    // create a comment 
+    public function store(Request $request, $id)
+    {
+        $post = Post::find($id);
+
+        if (!$post) {
+            return response([
+                'message' => 'post not found'
+            ], 403);
+        }
+        // validate comm 
+        $attrs = $request->validate([
+            'comment' => 'required|string'
+        ]);
+        Comment::create([
+            'comment' => $attrs['comment'],
+            'post_id' => $id,
+            'user_id' => auth()->user()->id
+        ]);
+        return response([
+            'message' => 'Comment created'
+        ], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $comment = Comment::find($id);
+        if (!$comment) {
+            return response([
+                'message' => 'Comment not found'
+            ], 403);
+        }
+
+        if ($comment->user_id != auth()->user()->id) {
+            return response([
+                'message' => 'Permission denied'
+            ], 403);
+        }
+        // validate comment 
+        $attrs = $request->validate([
+            'comment' => 'required|string'
+        ]);
+        $comment->update([
+            'comment' => $attrs['comment']
+        ]);
+        return response([
+            'message' => 'Comment updated'
+        ], 200);
+    }
+
+    // delete cmomment f
+    public function destroy(Request $request, $id)
+    {
+        $comment = Comment::find($id);
+        if (!$comment) {
+            return response([
+                'message' => 'Comment not found'
+            ], 403);
+        }
+
+        if ($comment->user_id != auth()->user()->id) {
+            return response([
+                'message' => 'Permission denied'
+            ], 403);
+        }
+        // validate comment 
+        $comment->delete();
+
+        return response([
+            'message' => 'Comment deleted'
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +29,23 @@ Route::get('/login', [AuthController::class, 'login']);
 Route::group(['middleware'=>['auth:sanctum']],
 function(){
    Route::post('/logout', [AuthController::class,'logout']);
-        Route::get('/user', [AuthController::class, 'user']); 
+        Route::get('/user', [AuthController::class, 'user']);
+        Route::put('/user', [AuthController::class, 'updateu']); 
+
+        // post 
+        Route::get('/posts',[PostController::class,'index']);
+        Route::post('/posts',[PostController::class,'store']);
+        Route::get('/posts/{id}',[PostController::class,'show']);
+        Route::put('/posts/{id}',[PostController::class,'update']);
+        Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+
+        // comments 
+        Route::get('/posts/{id}/comments',[CommentController::class,'index']); //all comments
+        Route::post('/posts/{id}/comments',[CommentController::class,'store']); //create comments
+        Route::put('comments/{id}',[CommentController::class,'update']); //create comments
+        Route::delete('comments/{id}', [CommentController::class, 'destroy']); //create comments
+        
+        // like 
+        Route::post('/posts/{id}/likes',[LikeController::class,'likeOrUnlike']);
 
 });
